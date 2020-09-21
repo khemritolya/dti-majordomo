803fe6921cfd1c5374dae614bb69f6ec738f6f63
Luis Hoderlein <lrh74@cornell.edu>
2020-09-21 16:15:19 -0400

    Basic slack functionality
    Ask me about it on slack!

---
+++ backend/server.ts
@@ -0,0 +1,86 @@
+import { WebClient } from "@slack/web-api";
+import express from "express";
+import bodyParser from "body-parser";
+import Vm from "vm.js";
+
+// A particular endpoint, and all the information associated with it.
+interface Endpoint {
+    url: string,
+    name: string,
+    onCallCode: string,
+    linkedGithubRepo: string,
+    linkedSlackChannel: string,
+}
+
+let majordomoSuggestionEndpoint: Endpoint = {
+    url: "something",
+    name: "dti-majordomo-suggestion", 
+    onCallCode: 
+    `let data = getResponseJson();
+    slackPost(\"Someone has a suggestion: \" + data.text);`,
+    linkedGithubRepo: "",
+    linkedSlackChannel: "#zzz-testing-channel"
+}
+
+// All the endpoints we have on the server
+// Why yes, I *am* too lazy to use a DB!
+var endpoints: Endpoint[] = [ majordomoSuggestionEndpoint ];
+
+// Check if we have a slack token
+if (!process.env.SLACK_TOKEN) {
+    console.log("Unable to find slack token!")
+} else {
+    console.log("Integrating with slack!");
+}
+
+
+// Our window into slack!
+// Used to push messages
+const slack = new WebClient(process.env.SLACK_TOKEN);
+
+
+// Very basic
+// Function which sends a slack message eventually.
+const sendSlackMessage = async function(channel: string, text: string) {
+    try {
+        await slack.chat.postMessage({
+            channel: channel,
+            text: text
+        });
+
+        console.log(`Sent slack message in ${channel}: ${text}`);
+    } catch (err) {
+        console.log(`An error has occured while trying to slack post:\n${err}`);
+    }
+};
+
+// Set up the express server
+const httpServer = express();
+httpServer.use(bodyParser.urlencoded({ extended: false }));
+httpServer.use(bodyParser.json());
+
+// Set up the ability to list for each endpoint
+endpoints.forEach(endpoint => {
+    httpServer.post(`/${endpoint.url}`, async (req, res) => {
+        console.log("You have reached the endpoint!");
+
+        // Create VM to run endpoint action code
+        const sandbox = new Vm();
+
+        // Define the integration functions that the user can call!
+        sandbox.realm.global.getResponseJson = function() { return req.body; };
+        sandbox.realm.global.slackPost = function(text: string) {
+            return (async () => { await sendSlackMessage(endpoint.linkedSlackChannel, text) })();
+        };
+
+        sandbox.eval(endpoint.onCallCode);
+
+        res.send({
+            "sucess": true
+        });
+    });
+})
+
+httpServer.listen(1776, async () => {
+    console.log("Starting Webserver on port 1776");
+});
