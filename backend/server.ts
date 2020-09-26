0748cca15548ec90ffa8bb05260eaf9a2acb5f21
Luis Hoderlein <lrh74@cornell.edu>
2020-09-25 22:36:16 -0400

    Gate endpoint queries/edits behind random string
    It's sent to slack, so only good folks should be able to see what the address is!

--- backend/server.ts
+++ backend/server.ts
@@ -2,6 +2,8 @@ import { WebClient } from "@slack/web-api";
 import express from "express";
 import bodyParser from "body-parser";
 import Vm from "vm.js";
+import crypto from "crypto";
+import ipify from "ipify";
 
 // A particular endpoint, and all the information associated with it.
 interface Endpoint {
@@ -12,14 +14,18 @@ interface Endpoint {
     linkedSlackChannel: string
 }
 
+// What slack channel do majordomo things post to?
+const majordomoTestingChannel: string = "#majordomo-testing-channel"
+
+// An example suggestions endpoint
 let majordomoSuggestionEndpoint: Endpoint = {
     url: "something",
-    name: "dti-majordomo-suggestion", 
-    onCallCode: 
+    name: "dti-majordomo-suggestion",
+    onCallCode:
     `let data = getResponseJson();
     slackPost(\"Someone has a suggestion: \" + data.text);`,
     linkedGithubRepo: "",
-    linkedSlackChannel: "#zzz-testing-channel",
+    linkedSlackChannel: majordomoTestingChannel,
 }
 
 let gitEndpoint: Endpoint = {
@@ -27,7 +33,7 @@ let gitEndpoint: Endpoint = {
     name: "dti-majordomo-git",
     onCallCode: `let data`,
     linkedGithubRepo: "https://github.com/ngwattcos/test-repo",
-    linkedSlackChannel: "#zzz-testing-channel",
+    linkedSlackChannel: majordomoTestingChannel,
 }
 
 // All the endpoints we have on the server
@@ -42,11 +48,9 @@ if (!process.env.SLACK_TOKEN) {
     console.log("Integrating with slack!");
 }
 
-
 // Our window into slack!
 // Used to push messages
 const slack = new WebClient(process.env.SLACK_TOKEN);
-
 
 // Very basic
 // Function which sends a slack message eventually.
@@ -62,6 +66,21 @@ const sendSlackMessage = async function(channel: string, text: string) {
         console.log(`An error has occured while trying to slack post:\n${err}`);
     }
 };
+
+// What port is this program running on?
+const port = 1776
+
+// Hide our endpoint creation endpoints behind this gargabe. We wouldn't want anyone to be able to create endpoints
+// Note that we DON'T add this to user created endpoints
+// anyone should be able to call those no problem
+const randomEndpointAddressModifier = crypto.randomBytes(30).toString("hex");
+
+// Let DTI know how they can contact the server!
+(async () => {
+    const addr = await ipify({useIPv6: false});
+
+    await sendSlackMessage(majordomoTestingChannel, `Majordomo is up at the following address: http://${addr}:${port}/${randomEndpointAddressModifier}/`);
+})();
 
 // Set up the express server
 const httpServer = express();
@@ -96,11 +115,18 @@ const setupEndpoints = () => {
 
 setupEndpoints();
 
-httpServer.get("/", (req, res) => {
-    console.log("received a thing");
-    res.send("here's the thing");
-})
-httpServer.post("/create-endpoint", async (req, res) => {
+httpServer.get(`/${randomEndpointAddressModifier}`, (req, res) => {
+    console.log("Someone tried to navigate to this page!");
+    res.send("Sorry mate, that's now how you access this service. Check the github/slack! Cheers");
+});
+
+// What to do if someone asks for all the endpoints that are available
+httpServer.get(`/${randomEndpointAddressModifier}/get-endpoints`, async (req, res) => {
+    res.send(endpoints);
+});
+
+// What to do to create a new endpoint
+httpServer.post(`/${randomEndpointAddressModifier}/create-endpoint`, async (req, res) => {
     let body = (req.body) as Endpoint;
 
     console.log(`Request to create endpoint ${body.name}`);
@@ -113,6 +139,6 @@ httpServer.post("/create-endpoint", async (req, res) => {
     res.send(`now have the following endpoints: ${Array.from(endpoints).reduce<Record<symbol, string>>((prev, curr) => `${prev}, ${curr.name}`, "")}`);
 })
 
-httpServer.listen(1776, async () => {
-    console.log("Starting Webserver on port 1776");
+httpServer.listen(port, async () => {
+    console.log(`Starting Webserver on port ${port}`);
 });
